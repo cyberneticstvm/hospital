299abbea5f54147dd92121a7eadda74e1a4f683a
vijoy sasidharan <devuinfosys@gmail.com>
2022-03-20 17:52:59 +0530

    Small update

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -10,6 +10,16 @@
                         <span class="text-muted"></span>
                     </div>
                 </div>
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                    </ul>
+                </div>
+                @endif
                 <div class="card">
                     <div class="card-body">
                         <form method="post" action="/roles/create/">
