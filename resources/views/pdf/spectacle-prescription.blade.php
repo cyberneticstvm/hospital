8c8ddf9f8e9662c2df4251576e346cb51bbee459
CYBER NETICS <cybernetics.me@outlook.com>
2022-04-27 10:43:56 +0530

    Small Updates

--- resources/views/pdf/spectacle-prescription.blade.php
+++ resources/views/pdf/spectacle-prescription.blade.php
@@ -61,7 +61,7 @@
     <center><p>EYE GLASS PRESCRIPTION</p></center>
     <div class="row">
         <div class="col">
-            <table width="99%" class="bordered" cellspacing="0" cellpadding="0">
+            <table width="98%" class="bordered" cellspacing="0" cellpadding="0">
                 <tr><td>RIGHT</td><td>SPH</td><td>CYL</td><td>AXIS</td><td>VA</td><td>PRISM</td></tr>
                 <tr><td>DIST.</td><td>{{ $spectacle->re_dist_sph }}</td><td>{{ $spectacle->re_dist_cyl }}</td><td>{{ $spectacle->re_dist_axis }}</td><td>{{ $spectacle->re_dist_va }}</td><td>{{ $spectacle->re_dist_prism }}</td></tr>
                 <tr><td>INT.</td><td>{{ $spectacle->re_int_sph }}</td><td>{{ $spectacle->re_int_cyl }}</td><td>{{ $spectacle->re_int_axis }}</td><td>{{ $spectacle->re_int_va }}</td><td>{{ $spectacle->re_int_prism }}</td></tr>
@@ -70,7 +70,7 @@
             </table>
         </div>
         <div class="col">
-            <table width="99%" class="bordered" cellspacing="0" cellpadding="0">
+            <table width="98%" class="bordered" cellspacing="0" cellpadding="0">
                 <tr><td>LEFT</td><td>SPH</td><td>CYL</td><td>AXIS</td><td>VA</td><td>PRISM</td></tr>
                 <tr><td>DIST.</td><td>{{ $spectacle->le_dist_sph }}</td><td>{{ $spectacle->le_dist_cyl }}</td><td>{{ $spectacle->le_dist_axis }}</td><td>{{ $spectacle->le_dist_va }}</td><td>{{ $spectacle->le_dist_prism }}</td></tr>
                 <tr><td>INT.</td><td>{{ $spectacle->le_int_sph }}</td><td>{{ $spectacle->le_int_cyl }}</td><td>{{ $spectacle->le_int_axis }}</td><td>{{ $spectacle->le_int_va }}</td><td>{{ $spectacle->le_int_prism }}</td></tr>
