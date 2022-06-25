20aa268bcef9eceb06157a2547867a78ad473024
CYBER NETICS <cybernetics.me@outlook.com>
2022-06-25 09:23:25 +0530

    error checking

--- public/js/consultation.js
+++ public/js/consultation.js
@@ -51,7 +51,6 @@ $(function(){
             },
             error: function(XMLHttpRequest, textStatus, errorThrown){
                 console.log(XMLHttpRequest)
-                alert(XMLHttpRequest);
             },
             beforeSend: function(){
                 $(".btn-consultation").html("<span class='spinner-grow spinner-grow-sm' role='status' aria-hidden='true'></span>&nbsp;Loading...");
