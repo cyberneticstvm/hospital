8f71d395d8e1531f57dac7307826205c99498f3b
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-09-19 23:02:28 +0530

    small updates

--- resources/views/pdf/medical-record.blade.php
+++ resources/views/pdf/medical-record.blade.php
@@ -223,8 +223,8 @@
         <tbody>
             @forelse($retinas as $key => $retina)
                 <tr>
-                    <td width="50%"><img src="{{ public_path().'/storage/'.$retina->retina_img }}" width='50%' /></td>
-                    <td width="50%"><img src="{{ 'https://hospital.speczone.net/public/storage/'.$retina->retina_img }}" width='50%' /></td>
+                    <td width="50%"><img src="{{ ($retina->retina_type == 'od') ? 'https://hospital.speczone.net/public/storage/'.$retina->retina_img : '' }}" width='50%' /></td>
+                    <td width="50%"><img src="{{ ($retina->retina_type == 'os') ? 'https://hospital.speczone.net/public/storage/'.$retina->retina_img : '' }}" width='50%' /></td>
                 </tr>
             @empty
             @endforelse
