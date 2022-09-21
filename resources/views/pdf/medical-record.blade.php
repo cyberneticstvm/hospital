d32786f2898419b11f07fb6b6730501f79fc21ce
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-09-21 19:19:36 +0530

    small updates

--- resources/views/pdf/medical-record.blade.php
+++ resources/views/pdf/medical-record.blade.php
@@ -341,7 +341,7 @@
     <br />
     @endif
     <table width="100%" class="no-border">
-        <tr><th>{{ ($record->is_patient_admission == 'sel') ? '' : 'Admission Advised:' }}</th><th>{{ ($record->is_patient_surgery == 'sel') ? '' : 'Surgery Advised:' }} </th><th>Review Date: </th></tr>
+        <tr><th>{{ ($record->is_patient_admission == 'sel') ? '' : 'Admission Advised:' }}</th><th>{{ ($record->is_patient_surgery == 'sel') ? '' : 'Surgery Advised:' }} </th><th class="text-right">Review Date: </th></tr>
         <tr><th>{{ ($record->is_patient_admission == 'sel') ? '' : $record->is_patient_admission }}</th><th>{{ ($record->is_patient_surgery == 'sel') ? '' : $record->is_patient_surgery }}</th><th class="text-right">{{ ($record->review_date) ? date('d/M/Y', strtotime($record->review_date)) : '' }}</th></tr>
     </table>
 </body>
