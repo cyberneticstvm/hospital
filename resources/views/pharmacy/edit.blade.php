389455d9d907dbfd080c070c1a0f58089cd14d8f
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-09-24 12:47:27 +0530

    small updates

--- resources/views/pharmacy/edit.blade.php
+++ resources/views/pharmacy/edit.blade.php
@@ -1,7 +1,7 @@
 @extends("templates.base")
 @section("content")
 <div class="body d-flex">
-    <div class="container">        
+    <div class="container-fluid">        
         <div class="row g-4">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <div class="d-flex flex-wrap justify-content-between align-items-end">
