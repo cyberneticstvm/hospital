abb9b4ce79b059cf5577d177eb39e2f96e552e4e
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-10-09 08:58:12 +0530

    small updates

--- public/js/spectacle.js
+++ public/js/spectacle.js
@@ -14,10 +14,10 @@ $(function(){
         if(int_add){
             var age = $("#age").val();
             var re_dist_sph = $(".re_dist_sph").val();
-            if(age < 52 && re_dist_add > 0){
+            if(age < 52 && re_dist_add > 0 && re_dist_sph){
                 var int_sph = (parseFloat(int_add)) ? parseFloat(int_add)+parseFloat(re_dist_sph): int_sph;
                 $(".re_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '0.00');
-            }else{
+            }else if(age < 52 && re_dist_add > 0 && re_dist_sph){
                 var int_sph = (parseFloat(int_add)) ? parseFloat(int_add)+parseFloat(re_dist_sph) : int_sph;
                 $(".re_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '0.00');
             }
@@ -34,10 +34,10 @@ $(function(){
         var re_near = (parseFloat(re_dist_sph)+parseFloat(re_dist_add) > 0) ? parseFloat(re_dist_sph)+parseFloat(re_dist_add) : parseFloat(re_dist_sph)+parseFloat(re_dist_add);
         $(".re_near_sph").val((re_near > 0) ? '+' + re_near.toFixed(2) : (re_near) ? re_near.toFixed(2) : '0.00');
 
-        if(age < 52 && re_dist_add > 0){
+        if(age < 52 && re_dist_add > 0 && re_dist_sph){
             var int_sph = (parseFloat(re_dist_sph)) ? parseFloat(re_dist_sph)+0.75 : 0.75;
             $(".re_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '0.00');
-        }else{
+        }else if(age > 52 && re_dist_add > 0 && re_dist_sph){
             var int_sph = (parseFloat(re_dist_sph)) ? parseFloat(re_dist_sph)+1.25 : 1.25;
             $(".re_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '0.00');
         }
@@ -66,10 +66,10 @@ $(function(){
         if(int_add){
             var age = $("#age").val();
             var le_dist_sph = $(".le_dist_sph").val();
-            if(age < 52 && re_dist_add > 0){
+            if(age < 52 && re_dist_add > 0 && le_dist_sph){
                 var int_sph = (parseFloat(int_add)) ? parseFloat(int_add)+parseFloat(le_dist_sph): int_sph;
                 $(".le_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '');
-            }else{
+            }else if(age > 52 && re_dist_add > 0 && le_dist_sph){
                 var int_sph = (parseFloat(int_add)) ? parseFloat(int_add)+parseFloat(le_dist_sph) : int_sph;
                 $(".le_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '');
             }
@@ -87,10 +87,10 @@ $(function(){
         var le_near = (parseFloat(le_dist_sph)+parseFloat(le_dist_add) > 0) ? parseFloat(le_dist_sph)+parseFloat(le_dist_add) : parseFloat(le_dist_sph)+parseFloat(le_dist_add);
         $(".le_near_sph").val((le_near > 0) ? '+' + le_near.toFixed(2) : (le_near) ? le_near.toFixed(2) : '0.00');
 
-        if(age < 52 && re_dist_add > 0){
+        if(age < 52 && re_dist_add > 0 && le_dist_sph){
             var int_sph = (parseFloat(le_dist_sph)) ? parseFloat(le_dist_sph)+0.75 : 0.75;
             $(".le_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '');
-        }else{
+        }else if(age > 52 && re_dist_add > 0 && le_dist_sph){
             var int_sph = (parseFloat(le_dist_sph)) ? parseFloat(le_dist_sph)+1.25 : 1.25;
             $(".le_int_sph").val((int_sph > 0) ? '+' + int_sph.toFixed(2) : (int_sph) ? int_sph.toFixed(2) : '');
         }
