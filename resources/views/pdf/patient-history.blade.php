f4a5b5742b8890c140bf0b82c55388ca7163d07d
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-10-29 09:36:03 +0530

    small updates

--- resources/views/pdf/patient-history.blade.php
+++ resources/views/pdf/patient-history.blade.php
@@ -135,6 +135,11 @@
         {{ ($mrecord->history) ? $mrecord->history : '' }}
     <br>
     @endif
+    @if($mrecord->allergic_drugs)
+    <p> Allergic to Drugs</p>
+        {{ ($mrecord->allergic_drugs) ? $mrecord->allergic_drugs : '' }}
+    <br>
+    @endif
     @if($spectacle && ($spectacle->re_dist_sph || $spectacle->re_dist_cyl || $spectacle->re_dist_axis || $spectacle->re_dist_add || $spectacle->vbr || $spectacle->re_near_va || $mrecord->va_od || $spectacle->le_dist_sph || $spectacle->le_dist_cyl || $spectacle->le_dist_axis || $spectacle->le_dist_add || $spectacle->vbl || $spectacle->le_near_va || $mrecord->va_os))
     <p>Vision</p>
     <table width="100%" class="table-bordered" cellspacing="0" cellpadding="0">
@@ -162,11 +167,6 @@
             </tr>
         </tbody>
     </table>
-    <br>
-    @endif
-    @if($mrecord->allergic_drugs)
-    <p> Allergic to Drugs</p>
-        {{ ($mrecord->allergic_drugs) ? $mrecord->allergic_drugs : '' }}
     <br>
     @endif
     @if($keratometry)
