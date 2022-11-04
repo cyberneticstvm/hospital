f4e9a0bcd249842aa39910accd29291f6c5b3311
CYBER NETICS <cybernetics.me@outlook.com>
2022-11-04 10:09:25 +0530

    small updates

--- resources/views/patient/search.blade.php
+++ resources/views/patient/search.blade.php
@@ -15,9 +15,9 @@
                         <form action="{{ route('patient.fetch') }}" method="post">
                             @csrf
                             <div class="row g-4">
-                                <div class="col-sm-4">
-                                    <label class="form-label">ID/Mobile/Name<sup class="text-danger">*</sup></label>
-                                    <input type="number" value="{{ $search_term }}" name="search_term" class="form-control form-control-md" placeholder="ID/Mobile/Name">
+                                <div class="col-sm-3">
+                                    <label class="form-label">Patient ID/Mobile/Name<sup class="text-danger">*</sup></label>
+                                    <input type="number" value="{{ $search_term }}" name="search_term" class="form-control form-control-md" placeholder="Patient ID/Mobile/Name">
                                     @error('search_term')
                                     <small class="text-danger">{{ $errors->first('search_term') }}</small>
                                     @enderror
