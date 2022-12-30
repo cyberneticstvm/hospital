8419a3f0efe43bfea57f6401db2ee7c18f128346
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-12-30 18:40:17 +0530

    small updates

--- resources/views/consultation/patient-reference.blade.php
+++ resources/views/consultation/patient-reference.blade.php
@@ -22,7 +22,7 @@
                     <td><a href='/generate-token/{{ $patient->reference_id }}/' target='_blank'><i class="fa fa-file text-info"></i></a></td>
                     <td><a href='/generate-prescription/{{ $patient->reference_id }}/' target='_blank'><i class="fa fa-file text-primary"></i></a></td>
                     <td><a href='/generate-receipt/{{ $patient->reference_id }}/' target="_blank"><i class="fa fa-file text-success"></i></a></td>
-                    <td class="text-center"><input type="checkbox" class="chkSms" {{ ($patient->sms == 1) ? 'checked' : '' }} /></td>
+                    <td class="text-center"><input type="checkbox" class="form-check-input chkSms" {{ ($patient->sms == 1) ? 'checked' : '' }} /></td>
                     <td><a class='btn btn-link' href="{{ route('patient_reference.edit', $patient->reference_id) }}"><i class="fa fa-pencil text-warning"></i></a></td>                    
                     <!--<td>
                         <form method="post" action="{{ route('patient_reference.delete', $patient->reference_id) }}">
