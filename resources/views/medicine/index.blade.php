d9d0c109965524633f1493849f3b35fe74c7ecd4
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-13 15:25:29 +0530

    small updates

--- resources/views/medicine/index.blade.php
+++ resources/views/medicine/index.blade.php
@@ -29,7 +29,7 @@
                         <form method="post" action="{{ route('medicine.delete', $med->medical_record_id) }}">
                             @csrf 
                             @method("DELETE")
-                            <button type="submit" class="btn btn-link" onclick="javascript: return confirm('Are you sure want to delete this Medicine Record?');"><i class="fa fa-trash text-danger"></i></button>
+                            <button type="submit" class="btn btn-link" onclick="return confirm('Are you sure want to delete this Medicine Record?');"><i class="fa fa-trash text-danger"></i></button>
                         </form>
                     </td>
                 </tr>
