806484d8b9bc34d761da628b5f49c2600639a5f5
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-31 17:52:17 +0530

    small updates

--- app/Http/Controllers/HFAController.php
+++ app/Http/Controllers/HFAController.php
@@ -28,7 +28,7 @@ class HFAController extends Controller
 
     public function index()
     {
-        $hfas = HFA::leftJoin('patient_medical_records AS m', 'h_f_a_s.medical_record_id', '=', 'm.id')->leftJoin('patient_registrations AS p', 'h_f_a_s.patient_id', '=', 'p.id')->selectRaw("h_f_a_s.*, p.patient_name, p.patient_id, h_f_a_s.medical_record_id")->where('h_f_a_s.branch', $this->branch)->whereIn('h_f_a_s.status', [1,2])->whereDate('h_f_a_s.created_at', Carbon::today())->orderByDesc("h_f_a_s.id")->get();
+        $hfas = HFA::leftJoin('patient_medical_records AS m', 'h_f_a_s.medical_record_id', '=', 'm.id')->leftJoin('patient_registrations AS p', 'h_f_a_s.patient_id', '=', 'p.id')->selectRaw("h_f_a_s.*, p.patient_name, p.patient_id, h_f_a_s.medical_record_id")->where('h_f_a_s.branch', $this->branch)->whereIn('h_f_a_s.status', [1,2])->orderByDesc("h_f_a_s.id")->get();
         return view('hfa.index', compact('hfas'));
     }
 
