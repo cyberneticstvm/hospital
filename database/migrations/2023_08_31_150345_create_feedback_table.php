8f27bace49d8f59411cb9fca760dcd694f7448ce
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-08-31 15:10:57 +0530

    small updates

---
+++ database/migrations/2023_08_31_150345_create_feedback_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->text('feedback')->nullable();
+            $table->string('type')->comment('complaint, enquiry, other')->nullable();
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
