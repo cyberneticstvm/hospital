c0c325a11e6683edaad768a82d4d11237e2dc3a0
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-08-31 13:58:20 +0530

    small updates

---
+++ database/migrations/2023_08_31_135442_alter_table_appointments.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->string('type', 5)->after('medical_record_id')->comment('web-hostpital, app-patient')->default('web');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+};
