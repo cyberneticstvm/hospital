2dbb393d5727b3b17d44bbc03fd36684773aece8
Cybernetics <mail@cybernetics.me>
2024-02-11 10:27:37 +0530

    small updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -377,8 +377,8 @@ class ReportController extends Controller
     public function fetchmRecord(Request $request)
     {
         $this->validate($request, [
-            'fromdate' => 'required',
-            'todate' => 'required',
+            'from_date' => 'required',
+            'to_date' => 'required',
         ]);
         $startDate = Carbon::parse($request->from_date)->startOfDay();
         $endDate = Carbon::parse($request->to_date)->endOfDay();
