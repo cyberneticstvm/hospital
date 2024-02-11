0b1adb790381e5b0c675576e9cf3ff9f0dab79c9
Cybernetics <mail@cybernetics.me>
2024-02-11 11:52:40 +0530

    small updates

--- resources/views/reports/tests-advised.blade.php
+++ resources/views/reports/tests-advised.blade.php
@@ -42,9 +42,9 @@
                                     <label class="form-label">Status </label>
                                     <select class="form-control form-control-md select2" data-placeholder="Select" name="status">
                                         <option value="">Select</option>
-                                        <option value="Pending" {{ ($input[3] == 'Pending') ? 'selected' : '' }}>Pending</option>
-                                        <option value="Completed" {{ ($input[3] == 'Completed') ? 'selected' : '' }}>Completed</option>
-                                        <option value="Cancelled" {{ ($input[3] == 'Cancelled') ? 'selected' : '' }}>Cancelled</option>
+                                        <option value="Pending" {{ ($inputs[3] == 'Pending') ? 'selected' : '' }}>Pending</option>
+                                        <option value="Completed" {{ ($inputs[3] == 'Completed') ? 'selected' : '' }}>Completed</option>
+                                        <option value="Cancelled" {{ ($inputs[3] == 'Cancelled') ? 'selected' : '' }}>Cancelled</option>
                                     </select>
                                 </div>
                                 <div class="col-sm-12 text-right">
