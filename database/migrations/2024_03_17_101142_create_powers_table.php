5d18e64bf9e4a51bda4aee70c77a6e0e57007ca3
Cybernetics <mail@cybernetics.me>
2024-03-17 10:14:07 +0530

    small updates

---
+++ database/migrations/2024_03_17_101142_create_powers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('powers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 5)->nullable();
+            $table->string('value', 7)->nullable();
+            $table->string('default', 10)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('powers');
+    }
+};
--- resources/views/pdf/prescription.blade.php
+++ resources/views/pdf/prescription.blade.php
@@ -1,49 +1,99 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Devi Eye Clinic & Opticians</title>
     <style>
-        .text-blue{
+        .text-blue {
             color: blue;
         }
-        table{
+
+        table {
             border-bottom: 1px solid #000;
         }
-        .text-medium{
+
+        .text-medium {
             font-size: 12px;
         }
-        .text-small{
+
+        .text-small {
             font-size: 10px;
         }
-        .text-large{
+
+        .text-large {
             font-size: 15px;
         }
     </style>
 </head>
+
 <body>
     <table width="100%" cellpadding='0' cellspacing='0' class="text-large">
         <tr>
-            <td width='20%'>MR.ID / Token</td><td width='50%'>{{ $reference->medical_record_id }} / {{ $reference->token }}</td>
+            <td width='20%'>MR.ID / Token</td>
+            <td width='50%'>{{ $reference->medical_record_id }} / {{ $reference->token }}</td>
             <td rowspan='3'>
-                <img src="./images/assets/Devi-Logo-Transparent.jpg" width='40%'/>
+                <img src="./images/assets/Devi-Logo-Transparent.jpg" width='40%' />
             </td>
         </tr>
         <tr>
-            <td>Patient Name</td><td>{{ strtoupper($patient->patient_name) }}</td>
+            <td>Patient Name</td>
+            <td>{{ strtoupper($patient->patient_name) }}</td>
         </tr>
         <tr>
-            <td>Patient ID</td><td>{{ $patient->patient_id }}</td>
+            <td>Patient ID</td>
+            <td>{{ $patient->patient_id }}</td>
         </tr>
         <tr>
-            <td width='20%'>Age</td><td>{{ $patient->age }} {{ ($patient->new_born_baby == 1) ? 'Months' : '' }} / {{ $patient->gender }}</td><td class='text-blue'>{{ strtoupper($doctor->doctor_name) }}</td>
+            <td width='20%'>Age</td>
+            <td>{{ $patient->age }} {{ ($patient->new_born_baby == 1) ? 'Months' : '' }} / {{ $patient->gender }}</td>
+            <td class='text-blue'>{{ strtoupper($doctor->doctor_name) }}</td>
         </tr>
         <tr>
-            <td>Address</td><td>{{ strtoupper($patient->address) }}</td><td class='text-blue'>{{ strtoupper($doctor->designation) }}</td>
+            <td>Address</td>
+            <td>{{ strtoupper($patient->address) }}</td>
+            <td class='text-blue'>{{ strtoupper($doctor->designation) }}</td>
         </tr>
     </table>
     <p class='text-right'>Date: {{ date('d/M/Y H:i:A') }}</p>
-    <pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre />
-    <pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre /><pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
+    <pre />
     <img src="data:image/png;base64, {!! $qrcode !!}">
     <p class="text-small">To book an appointment, scan this QR code.</p>
     <hr>
@@ -52,4 +102,5 @@
         <p>Ph: {{ $branch->contact_number }}, +91 9388611622</p>
     </center>
 </body>
+
 </html>
