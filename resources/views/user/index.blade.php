1dbce1e1ec1bf3053592a67f95e28ef7ba9dd74f
Cybernetics <mail@cybernetics.me>
2024-05-20 13:37:08 +0530

    small updates

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -8,35 +8,50 @@
 </div>
 <div class="card mb-4 border-0">
     <div class="card-body">
-        <p class= "text-right my-3"><a href="/user/create/"><i class="fa fa-plus fa-lg text-success"></i></a></p>
+        <p class="text-right my-3"><a href="/user/create/"><i class="fa fa-plus fa-lg text-success"></i></a></p>
         <table id="dataTbl" class="table display table-sm dataTable table-striped table-hover align-middle" style="width:100%">
-            <thead><tr><th>SL No.</th><th>Name</th><th>Username</th><th>Email</th><th>Roles</th><th>Mobile Access</th><th>Edit</th><!--<th>Remove</th>--></tr></thead><tbody>
-            @php $i = 0; @endphp
-            @foreach($users as $user)
+            <thead>
+                <tr>
+                    <th>SL No.</th>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Roles</th>
+                    <th>Mobile Access</th>
+                    <th>Status</th>
+                    <th>Edit</th>
+                    <th>Cancel</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $i = 0; @endphp
+                @foreach($users as $user)
                 <tr>
                     <td>{{ ++$i }}</td>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->username }}</td>
                     <td>{{ $user->email }}</td>
                     <td>
-                    @if(!empty($user->getRoleNames()))
+                        @if(!empty($user->getRoleNames()))
                         @foreach($user->getRoleNames() as $v)
                         <span class="badge bg-success">{{ $v }}</span>
                         @endforeach
-                    @endif
+                        @endif
                     </td>
                     <td class="text-center">{!! ($user->mobile_device == '0') ? "<i class='fa fa-times text-danger'></i>" : "<i class='fa fa-check text-primary'></i>" !!}</td>
+                    <td class="text-center">{!! ($user->deleted_at) ? "<i class='fa fa-times text-danger'></i>" : "<i class='fa fa-check text-primary'></i>" !!}</td>
                     <td><a class='btn btn-link' href="{{ route('user.edit', $user->id) }}"><i class="fa fa-pencil text-warning"></i></a></td>
-                    <!--<td>
+                    <td>
                         <form method="post" action="{{ route('user.delete', $user->id) }}">
-                            @csrf 
+                            @csrf
                             @method("DELETE")
-                            <button type="submit" class="btn btn-link" onclick="javascript: return confirm('Are you sure want to delete this Role?');"><i class="fa fa-trash text-danger"></i></button>
+                            <button type="submit" class="btn btn-link" onclick="javascript: return confirm('Are you sure want to Cancel this User?');"><i class="fa fa-trash text-danger"></i></button>
                         </form>
-                    </td>-->
+                    </td>
                 </tr>
-            @endforeach
-        </tbody></table>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
