b31a4ab440e3e8993ac0da6933315a269e4e58bc
Cybernetics <mail@cybernetics.me>
2024-10-07 11:04:25 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -24,10 +24,10 @@ class APIController extends Controller
             $prescription = Spectacle::selectRaw("CONCAT_WS(' / ', 'MRN', medical_record_id, DATE_FORMAT(created_at, '%d/%b/%Y')) AS mrn, id")->whereIn('medical_record_id', $mrns)->get();
             return response()->json([
                 'status' => true,
-                'mrecord' => $mrecord ?? 'Na',
-                'mrns' => $mrns ?? 'Na',
-                'patient' => $patient ?? 'Na',
-                'prescription' => $prescription ?? 'Na',
+                'mrecord' => 'Na',
+                'mrns' => 'Na',
+                'patient' => 'Na',
+                'prescription' => 'Na',
             ], 200);
         else :
             return response()->json([
