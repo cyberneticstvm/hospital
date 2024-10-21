7540ae2a896326933b684e63fcfc9bb32dc2a4a2
Cybernetics <mail@cybernetics.me>
2024-10-21 11:35:19 +0530

    small updates

--- resources/views/medicine/create.blade.php
+++ resources/views/medicine/create.blade.php
@@ -17,6 +17,11 @@
                             <input type="hidden" name="mid" value="{{ $medical_record->id }}" />
                             <input type="hidden" name="mrn" value="{{ $medical_record->mrn }}" />
                             <input type="hidden" name="" class="selFromBranch" value="{{ session()->get('branch') }}" />
+                            <input type='hidden' name='price[]' value='0.00' />
+                            <input type='hidden' name='discount[]' value='0.00' />
+                            <input type='hidden' name='tax_amount[]' value='0.00' />
+                            <input type='hidden' name='tax_percentage[]' value='0.00' />
+                            <input type='hidden' name='total[]' value='0.00' />
                             <div class="row g-4">
                                 <div class="col-sm-3">MRN: <h5 class="text-primary">{{ $medical_record->mrn }}</h5>
                                 </div>
@@ -26,32 +31,55 @@
                                 </div>
                                 <div class="col-sm-3">Doctor Name: <h5 class="text-primary">{{ $doctor->doctor_name }}</h5>
                                 </div>
-                                <div class="col-sm-3">
-                                    <label class="form-label">Medicine Advice</label>
-                                    <select class="form-control form-control-md select2" data-placeholder="Select" name="medicine_id[]">
-                                        <option value="">Select</option>
-                                        @foreach($medicines as $med)
-                                        <option value="{{ $med->id }}" {{ old('medicine_id') == $dia->id ? 'selected' : '' }}>{{ $med->product_name }}</option>
+                                <div class="col-sm-2">
+                                    <label class="form-label">Medicine Type</label>
+                                    <select class="form-control form-control-md select2 medType" name="medicine_type[]">
+                                        <option value="0">Select</option>
+                                        @foreach($mtypes as $medt)
+                                        <option value="{{ $medt->id }}" {{ old('type') == $medt->id ? 'selected' : '' }}>{{ $medt->name }}</option>
                                         @endforeach
+                                    </select>
+                                    @error('medicine_type')
+                                    <small class="text-danger">{{ $errors->first('medicine_type') }}</small>
+                                    @enderror
+                                </div>
+                                <div class="col-sm-3 asd">
+                                    <label class="form-label">Medicine Advised</label>
+                                    <select class="form-control form-control-md select2 medAdvised" name="medicine_id[]">
+                                        <option value="">Select</option>
+
                                     </select>
                                     @error('medicine_id')
                                     <small class="text-danger">{{ $errors->first('medicine_id') }}</small>
                                     @enderror
                                 </div>
-                                <div class="col-sm-2">
+                                <div class="col-sm-1">
                                     <label class="form-label">Dosage</label>
-                                    <input type="text" name="dosage[]" class="form-control form-control-md" placeholder="Eg: Daily 3 Drops" />
+                                    <input type="text" name="dosage[]" class="form-control form-control-md dos" placeholder="Eg: Daily 3 Drops" />
                                 </div>
-                                <div class="col-sm-2">
+                                <div class="col-sm-1">
+                                    <label class="form-label">Duration</label>
+                                    <input type="text" name="duration[]" class="form-control form-control-md dos" placeholder="Duration" />
+                                </div>
+                                <div class="col-sm-1">
+                                    <label class="form-label">Eye</label>
+                                    <select class="form-control form-control-md" name="eye[]">
+                                        <option value="B">Both</option>
+                                        <option value="R">RE</option>
+                                        <option value="L">LE</option>
+                                        <option value="O">Oral</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm-1">
                                     <label class="form-label">Qty / NOs.</label>
-                                    <input type='number' class='form-control form-control-md' name='qty[]' placeholder='0' />
+                                    <input type='number' class='form-control form-control-md qty' name='qty[]' placeholder='0' />
                                 </div>
                                 <div class="col-sm-2">
                                     <label class="form-label">Notes.</label>
                                     <input type='text' class='form-control form-control-md' name='notes[]' placeholder='Notes' />
                                 </div>
                                 <div class="col-sm-1">
-                                    <a class="medicineAdvise" href="javascript:void(0)"><i class="fa fa-plus fa-lg text-success"></i><a>
+                                    <a class="medicineAdvise" href="javascript:void(0)"><i class="fa fa-plus fa-lg text-success"></i></a>
                                 </div>
                                 <div class="medicineAdviseContainer">
 
