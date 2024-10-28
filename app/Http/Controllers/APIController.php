6012f2f325d021423b0faa41d23844fa8f7b4325
Cybernetics <mail@cybernetics.me>
2024-10-28 09:28:42 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -78,7 +78,7 @@ class APIController extends Controller
     function getCamps($secret)
     {
         if ($secret == $this->secret) :
-            $camps = CampMaster::whereDate('to', '>=', Carbon::today())->selectRaw("CONCAT_WS('-', camp_id, venue) AS name, id")->get();
+            $camps = CampMaster::whereDate('to', '>=', Carbon::today())->selectRaw("CONCAT_WS('-', camp_id, venue) AS name, id")->pluck('name', 'id');
             return response()->json([
                 'status' => true,
                 'camps' => collect($camps),
