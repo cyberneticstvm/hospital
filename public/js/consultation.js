6d287c664bcedb6fca51cb75ecdeb1af93d22d3b
Cybernetics <mail@cybernetics.me>
2025-01-01 16:26:35 +0530

    small updates

--- public/js/consultation.js
+++ public/js/consultation.js
@@ -60,7 +60,7 @@ $(function(){
 
     $(document).on('click', '.btn-consultation', function(e){
         e.preventDefault();
-        var form_data = $(this).closest('form').serializeArray();
+        var form_data = $(this).closest('form').serialize();
         var url = $(this).closest('form').attr('action');
         var btn = $("#btn_text").val();
         var vision_od_canvas = document.getElementById('re_eye');
