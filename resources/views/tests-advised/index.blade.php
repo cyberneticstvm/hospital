87c7f083662d89378ed626649a34ebdaf6f7861d
Cybernetics <mail@cybernetics.me>
2025-01-23 16:25:41 +0530

    small updates

--- resources/views/tests-advised/index.blade.php
+++ resources/views/tests-advised/index.blade.php
@@ -9,30 +9,46 @@
 <div class="card mb-4 border-0">
     <div class="card-body">
         @if(session()->has('success'))
-            <div class="alert alert-success">
-                {{ session()->get('success') }}
-            </div>
+        <div class="alert alert-success">
+            {{ session()->get('success') }}
+        </div>
         @endif
         <!--<p class= "text-right my-3"><a href="#"><i class="fa fa-plus fa-lg text-success"></i></a></p>-->
         <table id="dataTbl" class="table table-striped table-hover align-middle table-sm" style="width:100%">
-            <thead><tr><th>SL No.</th><th>MR ID.</th><th>Patient Name</th><th>Patient ID</th><th>Mobile</th><th>Doctor</th><th>Test</th><th>Notes</th><th>Proposed Date</th><th>Advised Date</th><th>Edit</th><!--<th>Remove</th>--></tr></thead><tbody>
-            @php $i = 0; @endphp
-            @foreach($tests as $test)
+            <thead>
+                <tr>
+                    <th>SL No.</th>
+                    <th>MR ID.</th>
+                    <th>Patient Name</th>
+                    <th>Patient ID</th>
+                    <th>Mobile</th>
+                    <th>Doctor</th>
+                    <th>Test</th>
+                    <th>Notes</th>
+                    <th>Proposed Date</th>
+                    <th>Advised Date</th>
+                    <th>Edit</th><!--<th>Remove</th>-->
+                </tr>
+            </thead>
+            <tbody>
+                @php $i = 0; @endphp
+                @foreach($tests as $test)
                 <tr>
                     <td>{{ ++$i }}</td>
                     <td>{{ $test->medical_record_id }}</td>
                     <td>{{ $test->patient->patient_name }}</td>
                     <td>{{ $test->patient->patient_id }}</td>
                     <td>{{ $test->patient->mobile_number }}</td>
-                    <td>{{ $test->doctor->doctor_name }}</td>
+                    <td>{{ $test?->doctor?->doctor_name }}</td>
                     <td>{{ $test->test()->find($test->test)->name }}</td>
                     <td>{{ $test->notes }}</td>
                     <td>{{ ($test->proposed_date) ? date('d/M/Y', strtotime($test->proposed_date)) : '' }}</td>
                     <td>{{ date('d/M/Y', strtotime($test->created_at)) }}</td>
                     <td><a class='btn btn-link' href="{{ route('tests.advised.edit', $test->id) }}"><i class="fa fa-pencil text-warning"></i></a></td>
                 </tr>
-            @endforeach
-        </tbody></table>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
