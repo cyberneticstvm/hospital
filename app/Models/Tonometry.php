54786df4fa60d0eb0616495062fa12e192d8075c
Cybernetics <mail@cybernetics.me>
2025-02-25 17:09:50 +0530

    small updates

--- app/Models/Tonometry.php
+++ app/Models/Tonometry.php
@@ -31,6 +31,6 @@ class Tonometry extends Model
 
     public function status()
     {
-        return ($this->deleted_at) ? "<span class='badge badge-danger'>Cancelled</span>" : "<span class='badge badge-info'>Active</span>";
+        return ($this->deleted_at) ? "<span class='badge badge-danger'>Cancelled</span>" : "<span class='badge badge-success text-success'>Active</span>";
     }
 }
