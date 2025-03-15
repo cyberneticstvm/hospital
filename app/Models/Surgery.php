4fe98088ac62fca2facc5379fa46297d8998027e
Cybernetics <mail@cybernetics.me>
2025-03-15 18:47:46 +0530

    small updates

--- app/Models/Surgery.php
+++ app/Models/Surgery.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Traits\HasRoles;
 
 class Surgery extends Model
 {
-    use HasFactory, HasRoles;
+    use HasFactory, HasRoles, SoftDeletes;
 
     protected $casts = ['surgery_date' => 'datetime'];
 
