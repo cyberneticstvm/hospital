16e9f2b72428618a225c7f5e55c777878b6aae30
Cybernetics <mail@cybernetics.me>
2025-03-26 23:01:22 +0530

    small updates

--- resources/views/procedure/create.blade.php
+++ resources/views/procedure/create.blade.php
@@ -1,7 +1,7 @@
 @extends("templates.base")
 @section("content")
 <div class="body d-flex">
-    <div class="container-fluid">        
+    <div class="container-fluid">
         <div class="row g-4">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <div class="d-flex flex-wrap justify-content-between align-items-end">
@@ -18,19 +18,26 @@
                             <input type="hidden" id="age" value="{{ $age }}" />
                             <input type="hidden" name="patient_id" value="{{ $patient->id }}" />
                             <div class="row g-4">
-                                <div class="col-sm-3">Medical Record No: <h5 class="text-primary">{{ $mrecord->id }}</h5></div>
-                                <div class="col-sm-3">Patient Name / Age: <h5 class="text-primary">{{ $patient->patient_name }} / {{ $age }}</h5></div>
-                                <div class="col-sm-3">Patient ID: <h5 class="text-primary">{{ $patient->patient_id }}</h5></div>
-                                <div class="col-sm-3">Doctor Name: <h5 class="text-primary">{{ $doctor->doctor_name }}</h5></div>
+                                <div class="col-sm-3">Medical Record No: <h5 class="text-primary">{{ $mrecord->id }}</h5>
+                                </div>
+                                <div class="col-sm-3">Patient Name / Age: <h5 class="text-primary">{{ $patient->patient_name }} / {{ $age }}</h5>
+                                </div>
+                                <div class="col-sm-3">Patient ID: <h5 class="text-primary">{{ $patient->patient_id }}</h5>
+                                </div>
+                                <div class="col-sm-3">Doctor Name: <h5 class="text-primary">{{ $doctor->doctor_name }}</h5>
+                                </div>
+                            </div>
+                            <div class="row g-4">
+                                <div class="col-md-12 text-center">Card Applied</div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-sm-9">
                                     <label class="form-label">Procedures<sup class="text-danger">*</sup> <small class="text-info">(Multiple selection enabled)</small></label>
                                     <select class="form-control form-control-md show-tick ms select2" multiple data-placeholder="Select" name="procedure[]">
-                                    <option value="">Select</option>
-                                    @foreach($procedures as $proc)
+                                        <option value="">Select</option>
+                                        @foreach($procedures as $proc)
                                         <option value="{{ $proc->id }}" {{ old('procedure') == $proc->id ? 'selected' : '' }}>{{ $proc->name }}</option>
-                                    @endforeach
+                                        @endforeach
                                     </select>
                                     @error('procedure')
                                     <small class="text-danger">{{ $errors->first('procedure') }}</small>
@@ -39,7 +46,7 @@
                             </div>
                             <div class="row g-4 mt-3">
                                 <div class="col-sm-12 text-right">
-                                    <button type="button" onClick="history.back()"  class="btn btn-danger">Cancel</button>
+                                    <button type="button" onClick="history.back()" class="btn btn-danger">Cancel</button>
                                     <button type="reset" class="btn btn-warning">Reset</button>
                                     <button type="submit" class="btn btn-primary btn-submit">Save</button>
                                 </div>
