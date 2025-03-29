6c10fa93d8c28592ad1d06c71f21c7f5d5cba2c5
Cybernetics <mail@cybernetics.me>
2025-03-29 09:14:33 +0530

    small updates

--- resources/views/reports/daybookcc.blade.php
+++ resources/views/reports/daybookcc.blade.php
@@ -75,6 +75,7 @@
                                     <th>SL No.</th>
                                     <th>Particulars</th>
                                     <th class="text-end">Total</th>
+                                    <th class="text-end">Discount</th>
                                     <th class="text-end">Receivables Today</th>
                                 </tr>
                             </thead>
@@ -84,77 +85,90 @@
                                     <td>Opening Balance</td>
                                     <td class="text-end text-danger">{{ number_format($opening_balance, 2) }}</td>
                                     <td></td>
+                                    <td></td>
                                 </tr>
                                 <tr>
                                     <td>2</td>
                                     <td>Income from Registration</td>
                                     <td class="text-end text-danger">{{ number_format($reg_fee_total, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger">{{ number_format($reg_fee_total, 2) }}</td>
                                 </tr>
                                 <tr>
                                     <td>3</td>
                                     <td>Income from Consultation</td>
                                     <td class="text-end">{{ number_format($consultation_fee_total, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger"><a class="daybook text-danger" href="javascript:void(0)" data-bs-toggle="modal" data-modal="consultationModal" data-bs-target="#consultationModal" data-title="Consultation Fee Detailed" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="consultation">{{ number_format($consultation_fee_total, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td>4</td>
                                     <td>Income from Procedures</td>
                                     <td class="text-end">{{ number_format($procedure_fee_total, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger"><a class="daybook text-danger" href="javascript:void(0)" data-bs-toggle="modal" data-modal="procedureModal" data-bs-target="#procedureModal" data-title="Procedure Fee Detailed" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="procedure">{{ number_format($procedure_fee_total, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td>5</td>
                                     <td>Income from Certificates</td>
                                     <td class="text-end">{{ number_format($certificate_fee_total, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger"><a class="daybook text-danger" href="javascript:void(0)" data-bs-toggle="modal" data-modal="certificateModal" data-bs-target="#certificateModal" data-title="Certificate Fee Detailed" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="certificate">{{ number_format($certificate_fee_total, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td>6</td>
                                     <td>Income from Pharmacy (Direct)</td>
                                     <td class="text-end text-danger">{{ number_format($pharmacy, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger"><a class="daybook text-danger" href="javascript:void(0)" data-bs-toggle="modal" data-modal="pharmacyModal" data-bs-target="#pharmacyModal" data-title="Pharmacy Income Detailed (Direct)" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="pharmacy">{{ number_format($pharmacy, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td>7</td>
                                     <td>Income from Pharmacy</td>
                                     <td class="text-end">{{ number_format($medicine, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger"><a class="daybook text-danger" href="javascript:void(0)" data-bs-toggle="modal" data-modal="medicineModal" data-bs-target="#medicineModal" data-title="Pharmacy Income Detailed" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="medicine">{{ number_format($medicine, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td>8</td>
                                     <td>Income from Vision</td>
                                     <td class="text-end text-danger">{{ number_format($vision, 2) }}</td>
+                                    <td></td>
                                     <td class="text-right text-danger"><a class="daybook text-danger" href="javascript:void(0)" data-bs-toggle="modal" data-modal="visionModal" data-bs-target="#visionModal" data-title="Vision Income Detailed" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="vision">{{ number_format($vision, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td>9</td>
                                     <td>Income from Clinical Lab</td>
                                     <td class="text-end">{{ number_format($clinical_lab, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">{{ number_format($clinical_lab, 2) }}</td>
                                 </tr>
                                 <tr>
                                     <td>10</td>
                                     <td>Income from Radiology Lab</td>
                                     <td class="text-end">{{ number_format($radiology_lab, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">{{ number_format($radiology_lab, 2) }}</td>
                                 </tr>
                                 <tr>
                                     <td>11</td>
                                     <td>Income from Surgery Medicine</td>
                                     <td class="text-end">{{ number_format($surgery_medicine, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">{{ number_format($surgery_medicine, 2) }}</td>
                                 </tr>
                                 <tr>
                                     <td>12</td>
                                     <td>Income from PostOp Medicine</td>
                                     <td class="text-end">{{ number_format($postop_medicine, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">{{ number_format($postop_medicine, 2) }}</td>
                                 </tr>
                                 <tr>
                                     <td>13</td>
                                     <td>Income from Surgery Consumables</td>
                                     <td class="text-end">{{ number_format($surgery_consumables, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">{{ number_format($surgery_consumables, 2) }}</td>
                                 </tr>
                                 <tr>
@@ -162,71 +176,83 @@
                                     <td>Income from Other Sources</td>
                                     <td class="text-end text-primary">{{ number_format($income, 2) }}</td>
                                     <td></td>
+                                    <td></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Grand Total</td>
                                     <td class="text-end fw-bold">{{ number_format($income_total, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end text-danger">{{ number_format($rtot, 2) }}</td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Expenses</td>
                                     <td class="text-end fw-bold">{{ number_format($expense, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end text-warning"></td>
                                 </tr>
                                 <tr>
-                                    <td colspan="4" class="bg-primary"></td>
+                                    <td colspan="5" class="bg-primary"></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Net Total</td>
                                     <td class="text-end fw-bold">{{ number_format($income_total - $expense, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end text-danger">{{ number_format($rtot, 2) }}</td>
                                 </tr>
                                 <tr>
-                                    <td colspan="4" class="bg-primary"></td>
+                                    <td colspan="5" class="bg-primary"></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Payments Received in Cash</td>
                                     <td class="text-end fw-bold">{{ number_format($income_received_cash, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end"><a class="daybook text-primary" href="javascript:void(0)" data-bs-toggle="modal" data-modal="incomeCashModal" data-bs-target="#incomeCashModal" data-title="Payments received in Cash" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="incomecash">{{ number_format($income_received_cash, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Payments received (UPI/Bank Transfer)</td>
                                     <td class="text-end fw-bold">{{ number_format($income_received_upi, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end"><a class="daybook text-primary" href="javascript:void(0)" data-bs-toggle="modal" data-modal="incomeUpiModal" data-bs-target="#incomeUpiModal" data-title="Payments received through UPI/Bank Transfer" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="incomeupi">{{ number_format($income_received_upi, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Payments received (Cheque/Card)</td>
                                     <td class="text-end fw-bold">{{ number_format($income_received_card, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end"><a class="daybook text-primary" href="javascript:void(0)" data-bs-toggle="modal" data-modal="incomeCardModal" data-bs-target="#incomeCardModal" data-title="Payments received through Card/Cheque" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="incomecard">{{ number_format($income_received_card, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Payments received (Staff)</td>
                                     <td class="text-end fw-bold">{{ number_format($income_received_staff, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">0.00</td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Patient Outstanding Due</td>
                                     <td class="text-end fw-bold">{{ number_format($outstanding, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">0.00</td>
                                 </tr>
 
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Patient Outstanding Received (UPI/Card/Transfer)</td>
                                     <td class="text-end fw-bold">{{ number_format($outstanding_received_other, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end"><a class="daybook text-primary" href="javascript:void(0)" data-bs-toggle="modal" data-modal="outstandingReceivedCardModal" data-bs-target="#outstandingReceivedCardModal" data-title="Outstanding Payments Received" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="outstandingReceived">{{ number_format($outstanding_received_other, 2) }}</a></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Patient Outstanding Received (Cash)</td>
                                     <td class="text-end fw-bold">{{ number_format($outstanding_received, 2) }}</td>
+                                    <td></td>
                                     <td class="text-end">{{ number_format($outstanding_received, 2) }}</td>
                                 </tr>
 
                                 <tr>
-                                    <td colspan="4" class="bg-primary"></td>
+                                    <td colspan="5" class="bg-primary"></td>
                                 </tr>
                                 <tr>
                                     <td colspan="2" class="text-end fw-bold">Balance</td>
                                     <td class="text-end fw-bold">{{ number_format($income_total+$outstanding_received-($income_received_upi + $income_received_card + $expense + $outstanding), 2) }}</td>
+                                    <td></td>
                                     <td class="text-end text-danger"><a class="daybook text-primary" href="javascript:void(0)" data-bs-toggle="modal" data-modal="incomePendingdModal" data-bs-target="#incomePendingdModal" data-title="Pending Payments" data-fdate="{{ ($inputs) ? $inputs[0] : $today }}" data-tdate="{{ ($inputs) ? $inputs[1] : $today }}" data-branch="{{ ($inputs && $inputs[2]) ? $inputs[2] : 0 }}" data-type="incomepending">{{ number_format(($rtot)-($income_received_cash + $income_received_upi + $income_received_card + $income_received_staff + $outstanding_received + $outstanding), 2) }}</a></td>
                                 </tr>
                             </tbody>
