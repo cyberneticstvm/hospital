77d77912785c159d4e8298b85dfe95bd6978ae71
Cybernetics <mail@cybernetics.me>
2025-03-30 10:24:25 +0530

    small updates

--- resources/views/pharmacy/index.blade.php
+++ resources/views/pharmacy/index.blade.php
@@ -6,29 +6,29 @@
         <span class="text-muted"></span>
     </div>
 </div>
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('pharmacy.direct.fetch') }}" method="post">
+            @csrf
+            <div class="row g-4">
+                <div class="col-sm-3">
+                    <label class="form-label">Medical Record ID (MR.ID)<sup class="text-danger">*</sup></label>
+                    <input type="number" value="{{ old('medical_record_number') }}" name="medical_record_number" class="form-control form-control-md" placeholder="Mediical Record Number" required="required">
+                    @error('medical_record_number')
+                    <small class="text-danger">{{ $errors->first('medical_record_number') }}</small>
+                    @enderror
+                </div>
+                <div class="col-sm-2">
+                    <label class="form-label">&nbsp;</label>
+                    <button type="submit" class="btn btn-primary btn-submit w-100">Fetch</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 <div class="card mb-4 border-0">
     <div class="card-body">
         <p class="text-right my-3"><a href="/extras/pharmacy/create/"><i class="fa fa-plus fa-lg text-success"></i></a></p>
-        <div class="card">
-            <div class="card-body">
-                <form action="{{ route('pharmacy.direct.fetch') }}" method="post">
-                    @csrf
-                    <div class="row g-4">
-                        <div class="col-sm-3">
-                            <label class="form-label">Medical Record ID (MR.ID)<sup class="text-danger">*</sup></label>
-                            <input type="number" value="{{ old('medical_record_number') }}" name="medical_record_number" class="form-control form-control-md" placeholder="Mediical Record Number" required="required">
-                            @error('medical_record_number')
-                            <small class="text-danger">{{ $errors->first('medical_record_number') }}</small>
-                            @enderror
-                        </div>
-                        <div class="col-sm-2">
-                            <label class="form-label">&nbsp;</label>
-                            <button type="submit" class="btn btn-primary btn-submit w-100">Fetch</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
         <table id="dataTbl" class="table display table-sm dataTable table-striped table-hover align-middle" style="width:100%">
             <thead>
                 <tr>
