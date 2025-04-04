a6ae96945caebc974e944d59fa992bd84d54399c
Cybernetics <mail@cybernetics.me>
2025-04-04 22:42:51 +0530

    small updates

--- resources/views/patient-payment/pharmacy-direct/edit.blade.php
+++ resources/views/patient-payment/pharmacy-direct/edit.blade.php
@@ -1,7 +1,7 @@
 @extends("templates.base")
 @section("content")
 <div class="body d-flex">
-    <div class="container">        
+    <div class="container">
         <div class="row g-4">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <div class="d-flex flex-wrap justify-content-between align-items-end">
@@ -19,9 +19,12 @@
                             <input type="hidden" name="patient_id" value="0" />
                             <input type="hidden" name="pharmacy_id" value="0" />
                             <div class="row g-4">
-                                <div class="col-sm-3">Bill No: <h5 class="text-primary">{{ $patient->id }}</h5></div>
-                                <div class="col-sm-3">Patient Name: <h5 class="text-primary">{{ $patient->patient_name }}</h5></div>
-                                <div class="col-sm-3">Date: <h5 class="text-primary">{{ date('d/M/Y', strtotime($patient->created_at)) }}</h5></div>
+                                <div class="col-sm-3">Bill No: <h5 class="text-primary">{{ $patient->id }}</h5>
+                                </div>
+                                <div class="col-sm-3">Patient Name: <h5 class="text-primary">{{ $patient->patient_name }}</h5>
+                                </div>
+                                <div class="col-sm-3">Date: <h5 class="text-primary">{{ date('d/M/Y', strtotime($patient->created_at)) }}</h5>
+                                </div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-sm-3">
@@ -30,8 +33,8 @@
                                         <input type="text" value="{{ ($payment->created_at) ? date('d/M/Y', strtotime($payment->created_at)) : '' }}" name="created_at" class="form-control form-control-md dtpicker">
                                         <div class="form-icon position-absolute">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-check" viewBox="0 0 16 16">
-                                                <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+                                                <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                                             </svg>
                                         </div>
                                     </fieldset>
@@ -46,17 +49,30 @@
                                     <small class="text-danger">{{ $errors->first('amount') }}</small>
                                     @enderror
                                 </div>
-                                <div class="col-sm-3">
+                                <div class="col-sm-2">
                                     <label class="form-label">Payment Mode<sup class="text-danger">*</sup></label>
                                     <select class="form-control select2" name="payment_mode">
                                         <option value="">Select</option>
                                         @forelse($pmodes as $key => $pmode)
-                                            <option value="{{ $pmode->id }}" {{ ($pmode->id == $payment->payment_mode) ? 'selected': '' }}>{{ $pmode->name }}</option>
+                                        <option value="{{ $pmode->id }}" {{ ($pmode->id == $payment->payment_mode) ? 'selected': '' }}>{{ $pmode->name }}</option>
                                         @empty
                                         @endforelse
                                     </select>
                                     @error('payment_mode')
                                     <small class="text-danger">{{ $errors->first('payment_mode') }}</small>
+                                    @enderror
+                                </div>
+                                <div class="col-sm-2">
+                                    <label class="form-label">Payment Type<sup class="text-danger">*</sup></label>
+                                    <select class="form-control select2" name="type">
+                                        <option value="">Select</option>
+                                        @forelse($types as $key => $type)
+                                        <option value="{{ $type->id }}" {{ ($type->id == $payment->type) ? 'selected': '' }}>{{ $type->name }}</option>
+                                        @empty
+                                        @endforelse
+                                    </select>
+                                    @error('type')
+                                    <small class="text-danger">{{ $errors->first('type') }}</small>
                                     @enderror
                                 </div>
                                 <div class="col-sm-4">
@@ -66,7 +82,7 @@
                             </div>
                             <div class="row g-4 mt-3">
                                 <div class="col-sm-12 text-right">
-                                    <button type="button" onClick="history.back()"  class="btn btn-danger">Cancel</button>
+                                    <button type="button" onClick="history.back()" class="btn btn-danger">Cancel</button>
                                     <button type="reset" class="btn btn-warning">Reset</button>
                                     <button type="submit" class="btn btn-primary btn-submit">Update</button>
                                 </div>
--- resources/views/patient-payment/pharmacy-direct/fetch.blade.php
+++ resources/views/patient-payment/pharmacy-direct/fetch.blade.php
@@ -1,7 +1,7 @@
 @extends("templates.base")
 @section("content")
 <div class="body d-flex">
-    <div class="container">        
+    <div class="container">
         <div class="row g-4">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <div class="d-flex flex-wrap justify-content-between align-items-end">
@@ -13,59 +13,105 @@
                 <div class="card">
                     <div class="card-body table-responsive">
                         <div class="row g-4 mb-3">
-                            <div class="col-sm-3">Bill No: <h5 class="text-primary">{{ $patient->id }}</h5></div>
-                            <div class="col-sm-3">Patient Name: <h5 class="text-primary">{{ $patient->patient_name }}</h5></div>
-                            <div class="col-sm-3">Date: <h5 class="text-primary">{{ date('d/M/Y', strtotime($patient->created_at)) }}</h5></div>
+                            <div class="col-sm-3">Bill No: <h5 class="text-primary">{{ $patient->id }}</h5>
+                            </div>
+                            <div class="col-sm-3">Patient Name: <h5 class="text-primary">{{ $patient->patient_name }}</h5>
+                            </div>
+                            <div class="col-sm-3">Date: <h5 class="text-primary">{{ date('d/M/Y', strtotime($patient->created_at)) }}</h5>
+                            </div>
                         </div>
                         <form action="{{ route('paypharma.save') }}" method="post">
                             @csrf
                             <input type="hidden" name="pharmacy_id" value="{{ $patient->id }}" />
                             <input type="hidden" name="branch" value="{{ $patient->branch }}" />
                             <table class="table table-sm table-striped table-hover align-middle">
-                                <thead><tr><th>Income Head</th><th>Amount</th></tr></thead>
+                                <thead>
+                                    <tr>
+                                        <th>Income Head</th>
+                                        <th>Amount</th>
+                                    </tr>
+                                </thead>
                                 <tbody>
-                                    <tr><td>Previous Due</td><td class="text-right"><a href="" target="_blank">0.00</a></td></tr>
-                                    <tr><td>Pharmacy Direct</td><td class="text-right">{{ number_format($amount, 2) }}</td></tr>
+                                    <tr>
+                                        <td>Previous Due</td>
+                                        <td class="text-right"><a href="" target="_blank">0.00</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Pharmacy Direct</td>
+                                        <td class="text-right">{{ number_format($amount, 2) }}</td>
+                                    </tr>
                                 </tbody>
                                 <tfoot>
-                                    <tr><th class="text-end fw-bold">Total</th><th class="text-end fw-bold">{{ number_format($amount, 2) }}</th></tr>
+                                    <tr>
+                                        <th class="text-end fw-bold">Total</th>
+                                        <th class="text-end fw-bold">{{ number_format($amount, 2) }}</th>
+                                    </tr>
                                 </tfoot>
                             </table>
                             <h5 class="mb-3">Payments received against Bill Number <span class="fw-bold">{{ $patient->id }}</span></h5>
                             <table class="table table-sm table-striped table-hover align-middle">
-                                <thead><tr><th>Payment Mode</th><th>Notes</th><th>Amount</th></tr></thead>
+                                <thead>
+                                    <tr>
+                                        <th>Payment Mode</th>
+                                        <th>Notes</th>
+                                        <th>Amount</th>
+                                    </tr>
+                                </thead>
                                 <tbody>
                                     @php $ptot = 0.00; @endphp
                                     @forelse($payments as $key => $payment)
-                                        <tr><td>{{ $payment->name }}</td><td>{{ $payment->notes }}</td><td class="text-right">{{ $payment->amount }}</td></tr>
+                                    <tr>
+                                        <td>{{ $payment->name }}</td>
+                                        <td>{{ $payment->notes }}</td>
+                                        <td class="text-right">{{ $payment->amount }}</td>
+                                    </tr>
                                     @php $ptot += $payment->amount; @endphp
                                     @empty
                                     @endforelse
                                 </tbody>
                                 <tfoot>
-                                    <tr><th colspan="2" class="text-end fw-bold">Received</th><th class="text-end fw-bold">{{ number_format($ptot, 2) }}</th></tr>
-                                    <tr><th colspan="2" class="text-end fw-bold">Balance</th><th class="text-end fw-bold">{{ number_format($amount-$ptot, 2) }}</th></tr>
+                                    <tr>
+                                        <th colspan="2" class="text-end fw-bold">Received</th>
+                                        <th class="text-end fw-bold">{{ number_format($ptot, 2) }}</th>
+                                    </tr>
+                                    <tr>
+                                        <th colspan="2" class="text-end fw-bold">Balance</th>
+                                        <th class="text-end fw-bold">{{ number_format($amount-$ptot, 2) }}</th>
+                                    </tr>
                                 </tfoot>
                             </table>
                             <div class="row g-4">
-                                <div class="col-sm-3">
+                                <div class="col-sm-2">
                                     <label class="form-label">Amount<sup class="text-danger">*</sup></label>
                                     <input type="number" name="amount" step="any" class="form-control form-control-md" placeholder="0.00">
                                     @error('amount')
                                     <small class="text-danger">{{ $errors->first('amount') }}</small>
                                     @enderror
                                 </div>
-                                <div class="col-sm-3">
+                                <div class="col-sm-2">
                                     <label class="form-label">Payment Mode<sup class="text-danger">*</sup></label>
                                     <select class="form-control select2" name="payment_mode">
                                         <option value="">Select</option>
                                         @forelse($pmodes as $key => $pmode)
-                                            <option value="{{ $pmode->id }}">{{ $pmode->name }}</option>
+                                        <option value="{{ $pmode->id }}">{{ $pmode->name }}</option>
                                         @empty
                                         @endforelse
                                     </select>
                                     @error('payment_mode')
                                     <small class="text-danger">{{ $errors->first('payment_mode') }}</small>
+                                    @enderror
+                                </div>
+                                <div class="col-sm-2">
+                                    <label class="form-label">Payment Type<sup class="text-danger">*</sup></label>
+                                    <select class="form-control select2" name="type">
+                                        <option value="">Select</option>
+                                        @forelse($types as $key => $type)
+                                        <option value="{{ $type->id }}">{{ $type->name }}</option>
+                                        @empty
+                                        @endforelse
+                                    </select>
+                                    @error('type')
+                                    <small class="text-danger">{{ $errors->first('type') }}</small>
                                     @enderror
                                 </div>
                                 <div class="col-sm-3">
