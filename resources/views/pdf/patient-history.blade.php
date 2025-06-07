7e7b02115a35c935fe466bfd70426cec27a9f34e
Cybernetics <mail@cybernetics.me>
2025-06-07 12:42:25 +0530

    small updates

--- resources/views/pdf/patient-history.blade.php
+++ resources/views/pdf/patient-history.blade.php
@@ -747,11 +747,11 @@
         <tr>
             <td>{{ $ont?->eye }}</td>
             <td>{{ $ont?->surgeond?->doctor_name }}</td>
-            <td>{{ $ont->date_of_surgery?->format('d.M.Y') }}</td>
-            <td>{{ $ont->test_dose_time?->format('h:i A') }}</td>
-            <td>{{ $ont->test_dose_result }}</td>
-            <td>{{ $ont->blood_pressure_mm }}/{{ $ont->blood_pressure_hg }} mmHg</td>
-            <td>{{ $ont->grbs }} mg/dL</td>
+            <td>{{ $ont?->date_of_surgery?->format('d.M.Y') }}</td>
+            <td>{{ $ont?->test_dose_time?->format('h:i A') }}</td>
+            <td>{{ $ont?->test_dose_result }}</td>
+            <td>{{ $ont?->blood_pressure_mm }}/{{ $ont->blood_pressure_hg }} mmHg</td>
+            <td>{{ $ont?->grbs }} mg/dL</td>
         </tr>
     </table>
     <br />
