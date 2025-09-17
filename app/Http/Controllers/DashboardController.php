43cf2202d87af678311f733d34fccb5e55dc9a29
Vijoy Sasidharan <mail@cybernetics.me>
2025-09-17 10:01:56 +0400

    small updates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -74,7 +74,7 @@ class DashboardController extends Controller
             $branches = DB::table('branches')->leftJoin('user_branches', 'branches.id', '=', 'user_branches.branch_id')->select('branches.id', 'branches.branch_name')->where('user_branches.user_id', '=', $user_id)->get();
             $sid = Str::random(25);
             User::where('id', $user_id)->update(['session_id' => $sid]);
-            //LoginLog::insert(['user_id' => $user_id, 'session_id' => $sid, 'ip' => $request->ip(), 'city_name' => $obj->city, 'region_name' => $obj->region, 'country_name' => $obj->country, 'zip_code' => $obj->postal, 'device' => $device, 'latitude' => $coordinates[0], 'longitude' => $coordinates[1], 'address' => $request->address, 'place_id' => $request->place_id, 'lat' => $request->lat, 'lng' => $request->lng, 'logged_in' => Carbon::now()]);
+            LoginLog::insert(['user_id' => $user_id, 'session_id' => $sid, 'ip' => $request->ip(), 'city_name' => null, 'region_name' => null, 'country_name' => null, 'zip_code' => null, 'device' => $device, 'latitude' => null, 'longitude' => null, 'address' => $request->address, 'place_id' => $request->place_id, 'lat' => $request->lat, 'lng' => $request->lng, 'logged_in' => Carbon::now()]);
             $branch_id = 0;
             $new_patients_count = 0;
             $review_count = 0;
