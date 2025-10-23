f42e95bee7604d4738e5e97d30ffdaa3b353ecda
Cybernetics <mail@cybernetics.me>
2025-10-23 15:32:36 +0530

    small updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Vijoy Sasidharan',
             'username' => 'vijo',
             'email' => 'mail@cybernetics.me',
-            'mobile' => '9188848860',
             'password' => bcrypt('stupid')
         ]);
 
