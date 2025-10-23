5a83b37a8a3453c12adb789608d2d8d680b9facc
Cybernetics <mail@cybernetics.me>
2025-10-23 15:31:43 +0530

    small updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Branch;
+use App\Models\User;
+use App\Models\UserBranch;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +20,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'Vijoy Sasidharan',
+            'username' => 'vijo',
+            'email' => 'mail@cybernetics.me',
+            'mobile' => '9188848860',
+            'password' => bcrypt('stupid')
+        ]);
+
+        $branch = Branch::create([
+            'branch_name' => 'Sasthamkotta',
+            'short_name' => 'SAS1',
+            'display_name' => 'Sasthamkotta',
+        ]);
+        $role = Role::create(['name' => 'Administrator']);
+        $permissions = Permission::pluck('id', 'id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+        UserBranch::create([
+            'user_id' => $user->id,
+            'branch_id' => $branch->id
+        ]);
     }
 }
