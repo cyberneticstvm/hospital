6dd3ddc48909c2f868731d2f929cfaf071cb43c9
Cybernetics <mail@cybernetics.me>
2025-11-19 16:35:46 +0530

    Small Updates

--- resources/views/pdf/prescription.blade.php
+++ resources/views/pdf/prescription.blade.php
@@ -47,7 +47,7 @@
                 @if(Helper::subdomain() == 'emrsas')
                 <img src="./images/assets/devi-sas-logo.png" width="35%" /><br />
                 @else
-                <img src="./images/assets/Devi-Logo-Transparent.jpg" width="25%" /><br />
+                <img src="./images/assets/Devi-Logo-Transparent.jpg" width="35%" /><br />
                 @endif
             </td>
         </tr>
