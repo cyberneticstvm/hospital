ef69e382fc7677e2f8a0596511dc714bb5f00bc4
Cybernetics <mail@cybernetics.me>
2025-11-21 18:24:21 +0530

    small updates

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -297,7 +297,7 @@ class HelperController extends Controller
     private function getConsultationDiscountDetailed($fdate, $tdate, $branch)
     {
         $consultation = DB::table('patient_medical_records as pmr')->leftJoin('patient_references as pr', 'pmr.mrn', '=', 'pr.id')->leftJoin('patient_registrations as preg', 'preg.id', '=', 'pr.patient_id')->select('preg.patient_name', 'preg.patient_id', 'pmr.id as mrid', 'pr.doctor_fee AS fee', 'pr.discount', 'pr.discount_notes as notes', 'pr.rc_number', DB::raw("DATE_FORMAT(pr.created_at, '%d/%b/%Y') AS rdate"))->whereBetween('pr.created_at', [$fdate, $tdate])->where('pr.branch', $branch)->where('pr.status', 1)->orderByDesc('pmr.id')->havingRaw('discount > ?', [0])->get();
-        $html = "<table class='table table-bordered table-striped table-hover table-sm'><thead><tr><th>SL No.</th><th>MR.ID</th><th>Patient Name</th><th>Patient ID</th><th>Reg.Date</th><th>Discount</th><th>Notes</th></tr></thead><tbody>";
+        $html = "<table class='table table-bordered table-striped table-hover table-sm'><thead><tr><th>SL No.</th><th>MR.ID</th><th>Patient Name</th><th>Patient ID</th><th>Reg.Date</th><th>Notes</th><th>Discount</th></tr></thead><tbody>";
         $c = 1;
         $tot = 0;
         foreach ($consultation as $key => $record) :
