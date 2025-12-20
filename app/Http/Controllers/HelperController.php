7d9f5596ce960a1e2114776f26477ea9b7013c1b
Cybernetics <mail@cybernetics.me>
2025-12-20 10:54:06 +0530

    small updates

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -544,7 +544,7 @@ class HelperController extends Controller
         $income = DB::table('patient_payments as p')->leftjoin('patient_registrations as preg', 'preg.id', '=', 'p.patient_id')->select('preg.patient_name', 'preg.patient_id', 'p.medical_record_id as mrid', 'p.amount as fee', DB::raw("DATE_FORMAT(p.created_at, '%d/%b/%Y') AS cdate"))->where('p.branch', $branch)->whereBetween('p.created_at', [$fdate, $tdate])->whereIn('p.payment_mode', [3, 4])->where('p.type', '!=', 9)->where('p.type', '!=', 8)->get();
         $c = 1;
         $tot = 0;
-        $html = "<table class='table table-bordered table-striped table-hover table-sm'><thead><tr><th>SL No.</th><th>MR.ID</th><th>Patient Name</th><th>Patient ID</th><th>Date</th><th>Amount</th></tr></thead><tbody>";
+        $html = "<table class='table table-bordered table-striped table-hover table-sm'><thead><tr><th>SL No.</th><th>MR.ID</th><th>Patient Name</th><th>Patient IDs</th><th>Date</th><th>Amount</th></tr></thead><tbody>";
         foreach ($income as $key => $record) :
             $html .= "<tr>";
             $html .= "<td>" . $c++ . "</td>";
