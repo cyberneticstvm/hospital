3e8912fad3386f6c28eda0e980f98342fcb641a4
Cybernetics <mail@cybernetics.me>
2026-01-01 15:55:05 +0530

    small updates

--- resources/views/pdf/b2breceipt.blade.php
+++ resources/views/pdf/b2breceipt.blade.php
@@ -39,65 +39,65 @@
         Registered Office: Maithanam, Varkala, Thiruvananthapuram. Phone: {{ $branch->contact_number }}<br>
         Branch: Thekkumbagom, Paravur, Kollam.<br>
         DL Number: RLF21KL2022000540, RLF20KL2022000543<br>
-        GSTIN: 32AHTPC8761A1ZH<
-            </center>
-            <br />
-            <table width="100%" border="1" cellspacing="0" cellpadding="0">
-                <thead>
-                    <tr>
-                        <th text-align="center" colspan="4">PHARMACY SALE BILL</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Customer Name</td>
-                        <td colspan="3">{{ $customer?->name }}</td>
-                    </tr>
-                    <tr>
-                        <td>Address</td>
-                        <td colspan="3">{{ $customer?->address }}</td>
-                    </tr>
-                    <tr>
-                        <td>Bill/Receipt No.</td>
-                        <td colspan="3">{{ $pharmacy->id }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <br />
-            <table class="bordered" width="100%" cellspacing="0" cellpadding="0">
-                <tbody>
-                    <tr>
-                        <td width="5%">SLNo.</td>
-                        <td width="30%">MEDICINE</td>
-                        <td width="10%">BATCH</td>
-                        <td width="5%">QTY</td>
-                        <td width="10%">PRICE</td>
-                        <td width="10%">DISCOUNT</td>
-                        <td width="10%">TAX%</td>
-                        <td width="10%">TAX AMOUNT</td>
-                        <td width="10%">TOTAL</td>
-                    </tr>
-                    @php $c = 1; $tot = 0.00; @endphp
-                    @foreach($medicines as $medicine)
-                    <tr>
-                        <td>{{ $c++ }}</td>
-                        <td>{{ $medicine->product_name }}</td>
-                        <td>{{ $medicine->batch_number }}</td>
-                        <td class="text-right">{{ $medicine->qty }}</td>
-                        <td class="text-right">{{ $medicine->price }}</td>
-                        <td class="text-right">{{ $medicine->discount }}</td>
-                        <td class="text-right">{{ $medicine->tax }}</td>
-                        <td class="text-right">{{ $medicine->tax_amount }}</td>
-                        <td class="text-right">{{ number_format($medicine->total + $medicine->tax_amount, 2) }}</td>
-                    </tr>
-                    @php $tot += $medicine->total + $medicine->tax_amount @endphp
-                    @endforeach
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="8" class="text-right"><strong>Total</strong></td>
-                        <td class="text-right"><strong>{{ number_format($tot, 2) }}</strong></td>
-                    </tr>
-                </tfoot>
-            </table>
+        GSTIN: 32AHTPC8761A1ZH
+    </center>
+    <br />
+    <table width="100%" border="1" cellspacing="0" cellpadding="0">
+        <thead>
+            <tr>
+                <th text-align="center" colspan="4">PHARMACY SALE BILL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Customer Name</td>
+                <td colspan="3">{{ $customer?->name }}</td>
+            </tr>
+            <tr>
+                <td>Address</td>
+                <td colspan="3">{{ $customer?->address }}</td>
+            </tr>
+            <tr>
+                <td>Bill/Receipt No.</td>
+                <td colspan="3">{{ $pharmacy->id }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <br />
+    <table class="bordered" width="100%" cellspacing="0" cellpadding="0">
+        <tbody>
+            <tr>
+                <td width="5%">SLNo.</td>
+                <td width="30%">MEDICINE</td>
+                <td width="10%">BATCH</td>
+                <td width="5%">QTY</td>
+                <td width="10%">PRICE</td>
+                <td width="10%">DISCOUNT</td>
+                <td width="10%">TAX%</td>
+                <td width="10%">TAX AMOUNT</td>
+                <td width="10%">TOTAL</td>
+            </tr>
+            @php $c = 1; $tot = 0.00; @endphp
+            @foreach($medicines as $medicine)
+            <tr>
+                <td>{{ $c++ }}</td>
+                <td>{{ $medicine->product_name }}</td>
+                <td>{{ $medicine->batch_number }}</td>
+                <td class="text-right">{{ $medicine->qty }}</td>
+                <td class="text-right">{{ $medicine->price }}</td>
+                <td class="text-right">{{ $medicine->discount }}</td>
+                <td class="text-right">{{ $medicine->tax }}</td>
+                <td class="text-right">{{ $medicine->tax_amount }}</td>
+                <td class="text-right">{{ number_format($medicine->total + $medicine->tax_amount, 2) }}</td>
+            </tr>
+            @php $tot += $medicine->total + $medicine->tax_amount @endphp
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="8" class="text-right"><strong>Total</strong></td>
+                <td class="text-right"><strong>{{ number_format($tot, 2) }}</strong></td>
+            </tr>
+        </tfoot>
+    </table>
 </body>
