ad2c098f1583e85748d1cdaf549af3aa6e9b44d3
Cybernetics <mail@cybernetics.me>
2026-02-18 09:11:04 +0530

    first commit

--- resources/views/patient/search.blade.php
+++ resources/views/patient/search.blade.php
@@ -76,7 +76,7 @@
                                 </tr>
                                 @empty
                                 <tr>
-                                    <td colspan="11" class="text-center"><a href="{{ route('appointment.create', ['mobile' => $search_term]) }}">Create Appointment</a></td>
+                                    <td colspan="11" class="text-center"><a href="{{ route('appointment.create', ['mobile' => $search_term]) }}">Create Appointment</a> | <a href="{{ route('appointment.create', ['mobile' => $search_term]) }}">Register Patient</a></td>
                                 </tr>
                                 @endforelse
                             </tbody>
